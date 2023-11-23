da1b3e1eb927ea56f41cec12f214dfcf9769e3c5
Маркозубова Анастасия <144169893+mmmilkinn@users.noreply.github.com>
2023-11-23 13:57:22 +0300

    Update rectangle_tests.py

--- rectangle_tests.py
+++ rectangle_tests.py
@@ -3,25 +3,25 @@ import rectangle
 
 class RectangleTestCase(unittest.TestCase):
     def test_zero_area(self):
-        res = rectangle.area(10, 0)
-        self.assertRaises(Value Error, area, 0)
+        res = rectangle.area(-10, 5)
+        self.assertRaises(Value Error, area, -5)
 
-    def test_rectahgle_area(self):
+    def test_rectangle_area(self):
         res = rectangle.area(100, 100)
         self.assertEqual(res, 10000)
 
-    def test_rectahgle_area(self):
+    def test_rectangle_area(self):
         res = rectangle.area(0, 100)
-        self.assertEqual(res, 10000)
+        self.assertRaises(Value Error, area, 0)
 
     def test_zero_perimeter(self):
-        res = rectangle.perimeter(0, 5)
-        self.assertRaises(Value Error, perimetr, 0)
+        res = rectangle.perimeter(4, -5)
+        self.assertRaises(Value Error, perimetr, -5)
 
     def test_rectangle_perimeter(self):
         res = rectangle.perimeter(10, 10)
         self.assertEqual(res, 40)
 
     def test_rectangle_perimetr(self):
-        res = perimeter(5, 5)
-        self.assertEqual(res, 20)
+        res = perimeter(5, 0)
+       self.assertRaises(Value Error, perimetr, 0)
