f193e7d94984b37f2fbb708658832d2f8a67412a
Маркозубова Анастасия <144169893+mmmilkinn@users.noreply.github.com>
2023-11-23 14:19:32 +0300

    Update triangle_tests.py

--- triangle_tests.py
+++ triangle_tests.py
@@ -7,21 +7,21 @@ class TestTriangleCase(unittest.TestCase):
         self.assertEqual(res, 30)
 
     def test_zero_area(self):
-        res = triangle.area(0, 5)
-        self.assertRaises(Value Error, area, 0)
+        with self.assertRaises(ValueError):
+            res = triangle.area(0)
 
     def test_negative_side_triangle_area(self):
-        res = area(4, -5)
-        self.assertRaises(Value Error, area, -5)
+        with self.assertRaises(ValueError):
+            res = triangle.area(0)
 
     def test_positive_perimeter(self):
         res = triangle.perimeter(10, 5, 8)
         self.assertEqual(res, 23)
 
     def test_zero_perimetr(self):
-        res = triangle.perimeter(5, 3, 0)
-        self.assertRaises(Value Error, perimetr, 0)
+        with self.assertRaises(ValueError):
+            res = triangle.perimetr(0)
 
     def test_negative_side_triangle_perimetr(self):
-        res = perimeter(4, 5, -6)
-        self.assertRaises(Value Error, perimetr, -6)
+        with self.assertRaises(ValueError):
+            res = triangle.perimetr(0)
