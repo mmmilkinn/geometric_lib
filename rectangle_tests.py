c23c0a25f207ab365444c3c34d93240e57571fec
Маркозубова Анастасия <144169893+mmmilkinn@users.noreply.github.com>
2023-11-23 13:17:55 +0300

    Update rectangle_tests.py

--- rectangle_tests.py
+++ rectangle_tests.py
@@ -4,24 +4,24 @@ import rectangle
 class RectangleTestCase(unittest.TestCase):
     def test_zero_area(self):
         res = rectangle.area(10, 0)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, Value Error)
 
-    def test_square_area(self):
+    def test_rectahgle_area(self):
         res = rectangle.area(100, 100)
         self.assertEqual(res, 10000)
 
-    def test_negative_side_rectangle_area(self):
-        res = area(5, -5)
-        self.assertEqual(res, 'The sides of the rectangle cannot be negative')
+    def test_rectahgle_area(self):
+        res = rectangle.area(0, 100)
+        self.assertEqual(res, 10000)
 
     def test_zero_perimeter(self):
         res = rectangle.perimeter(0, 5)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, Value Error)
 
-    def test_square_perimeter(self):
+    def test_rectangle_perimeter(self):
         res = rectangle.perimeter(10, 10)
         self.assertEqual(res, 40)
 
-    def test_negative_side_rectangle_perimetr(self):
-        res = perimeter(5, -5)
-        self.assertEqual(res, 'The sides of the rectangle cannot be negative')
+    def test_rectangle_perimetr(self):
+        res = perimeter(5, 5)
+        self.assertEqual(res, 20)
