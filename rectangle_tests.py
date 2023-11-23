3d5abe3e86bf3118d0a2cb916110e0776a022e37
Маркозубова Анастасия <144169893+mmmilkinn@users.noreply.github.com>
2023-11-23 14:15:04 +0300

    Update rectangle_tests.py

--- rectangle_tests.py
+++ rectangle_tests.py
@@ -3,25 +3,25 @@ import rectangle
 
 class RectangleTestCase(unittest.TestCase):
     def test_zero_area(self):
-        res = rectangle.area(-10, 5)
-        self.assertRaises(Value Error, area, -5)
+        res = rectangle.area(10, 0)
+        self.assertEqual(res, 0)
 
-    def test_rectangle_area(self):
-        res = rectangle.area(100, 100)
-        self.assertEqual(res, 10000)
+    def test_positive_area(self):
+        with self.assertRaises(ValueError):
+            res = rectangle.area(0)
 
-    def test_rectangle_area(self):
-        res = rectangle.area(0, 100)
-        self.assertRaises(Value Error, area, 0)
+    def test_negative_side_rectangle_area(self):
+        with self.assertRaises(ValueError):
+            res = rectangle.area(0)
 
     def test_zero_perimeter(self):
-        res = rectangle.perimeter(4, -5)
-        self.assertRaises(Value Error, perimetr, -5)
+        with self.assertRaises(ValueError):
+            res = rectangle.perimetr(0)
 
-    def test_rectangle_perimeter(self):
+    def test_square_perimeter(self):
         res = rectangle.perimeter(10, 10)
         self.assertEqual(res, 40)
 
-    def test_rectangle_perimetr(self):
-        res = perimeter(5, 0)
-       self.assertRaises(Value Error, perimetr, 0)
+    def test_negative_side_rectangle_perimetr(self):
+        with self.assertRaises(ValueError):
+            res = rectangle.area(0)
