b93eb03310d737dca4257c44e54d93a6e680bc16
Маркозубова Анастасия <144169893+mmmilkinn@users.noreply.github.com>
2023-11-23 14:12:32 +0300

    Update circle_tests.py

--- circle_tests.py
+++ circle_tests.py
@@ -7,21 +7,23 @@ class CircleTestCase(unittest.TestCase):
         self.assertAlmostEqual(res, 314.1592653589793)
 
     def test_zero_area(self):
-        res = circle.area(0)
-        self.assertRaises(Value Error, area, 0)
+        with self.assertRaises(ValueError):
+             res = circle.area(0)
+
 
     def test_negative_area(self):
-        res = circle.perimeter(-10)
-        self.assertRaises(Value Error, area, -10)
+        with self.assertRaises(ValueError):
+            res = circle.perimeter(-10)
 
     def test_positive_perimeter(self):
         res = circle.perimeter(10)
         self.assertAlmostEqual(res, 62.83185307179586)
 
     def test_zero_perimeter(self):
-        res = circle.perimeter(0)
-        self.assertRaises(Value Error, perimetr, 0)
+        with self.assertRaises(ValueError):
+            res = circle.perimeter(0)
+
 
     def test_negative_perimetr(self):
-        res = circle.perimeter(-10)
-        self.assertRaises(Value Error, perimetr, -10)
+        with self.assertRaises(ValueError):
+            res = circle.perimeter(-10)
